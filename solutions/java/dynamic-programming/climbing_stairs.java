19a43b81953f4a68b77c270f075df528f85b0e07
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-18 07:48:25 +0000

    ✅ Solved: climbing stairs (java)

---
+++ solutions/java/dynamic-programming/climbing_stairs.java
@@ -0,0 +1,10 @@
+/*
+ * Climbing Stairs
+ * Count ways to climb n stairs (1 or 2 steps)
+ * Category: dynamic-programming | Difficulty: Easy
+ * Date: 2026-02-18 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Climbing Stairs
