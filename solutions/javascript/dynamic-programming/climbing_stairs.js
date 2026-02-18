980fe56034d1df5fdb9b5365bd5d09ffcea83d3b
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-18 04:08:52 +0000

    ✅ Solved: climbing stairs (javascript)

---
+++ solutions/javascript/dynamic-programming/climbing_stairs.js
@@ -0,0 +1,8 @@
+/*
+ * Climbing Stairs
+ * Count ways to climb n stairs (1 or 2 steps)
+ * Category: dynamic-programming | Difficulty: Easy
+ * Date: 2026-02-18 | Author: Jay Singh
+ */
+
+// TODO: Implement Climbing Stairs
