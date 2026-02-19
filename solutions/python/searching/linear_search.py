4358a0eb535fe08c0ddebd358fb9f4187e0f64bc
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-19 07:47:55 +0000

    ✅ Solved: linear search (python)

---
+++ solutions/python/searching/linear_search.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Linear Search
+Implement linear search algorithm
+Category: searching | Difficulty: Easy
+Date: 2026-02-19 | Author: Jay Singh
+"""
+
+// TODO: Implement Linear Search
