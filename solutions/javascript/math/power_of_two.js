721b3a275aaf41bd5e497ff415b427bf9c79024b
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-20 07:45:19 +0000

    ✅ Solved: power of two (javascript)

---
+++ solutions/javascript/math/power_of_two.js
@@ -0,0 +1,8 @@
+/*
+ * Power of Two Check
+ * Check if number is power of 2 using bit manipulation
+ * Category: math | Difficulty: Easy
+ * Date: 2026-02-20 | Author: Jay Singh
+ */
+
+// TODO: Implement Power of Two Check
