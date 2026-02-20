65092e652d196abd600e04393070a86afe435a33
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-20 04:03:38 +0000

    ✅ Solved: stack implementation (javascript)

---
+++ solutions/javascript/data-structures/stack_implementation.js
@@ -0,0 +1,8 @@
+/*
+ * Stack Implementation
+ * Implement stack data structure with push, pop, peek
+ * Category: data-structures | Difficulty: Easy
+ * Date: 2026-02-20 | Author: Jay Singh
+ */
+
+// TODO: Implement Stack Implementation
