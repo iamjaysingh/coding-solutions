a6f94f4ec709dfeb2bb3d750aecbe71bbb1624d8
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-21 07:30:02 +0000

    ✅ Solved: linear search (java)

---
+++ solutions/java/searching/linear_search.java
@@ -0,0 +1,10 @@
+/*
+ * Linear Search
+ * Implement linear search algorithm
+ * Category: searching | Difficulty: Easy
+ * Date: 2026-02-21 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Linear Search
