cbf1f321d4978ad3ea387888a0ee07e848ea3070
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-22 07:33:35 +0000

    ✅ Solved: insertion sort (python)

---
+++ solutions/python/sorting/insertion_sort.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Insertion Sort
+Implement insertion sort algorithm
+Category: sorting | Difficulty: Easy
+Date: 2026-02-22 | Author: Jay Singh
+"""
+
+// TODO: Implement Insertion Sort
