629238956ba4233032d79a996bb8031ad74f4fc5
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-22 04:05:12 +0000

    ✅ Solved: armstrong number (python)

---
+++ solutions/python/math/armstrong_number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Armstrong Number
+Check if number is Armstrong number
+Category: math | Difficulty: Easy
+Date: 2026-02-22 | Author: Jay Singh
+"""
+
+// TODO: Implement Armstrong Number
