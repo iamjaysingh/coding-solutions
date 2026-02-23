49a9fbf2271f721729a87f848c891ac76ce34fbc
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-23 04:10:55 +0000

    ✅ Solved: merge sort (go)

---
+++ solutions/go/sorting/merge_sort.go
@@ -0,0 +1,12 @@
+/*
+ * Merge Sort
+ * Implement merge sort using divide and conquer
+ * Category: sorting | Difficulty: Medium
+ * Date: 2026-02-23 | Author: Jay Singh
+ */
+
+package main
+
+import "fmt"
+
+// TODO: Implement Merge Sort
