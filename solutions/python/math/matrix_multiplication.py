2267f07cadbb053c97b398c9b8a85f26f65ac055
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-23 07:52:28 +0000

    ✅ Solved: matrix multiplication (python)

---
+++ solutions/python/math/matrix_multiplication.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Matrix Multiplication
+Multiply two matrices
+Category: math | Difficulty: Medium
+Date: 2026-02-23 | Author: Jay Singh
+"""
+
+// TODO: Implement Matrix Multiplication
