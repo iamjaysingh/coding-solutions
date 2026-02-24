09e52bec55d640f5ef1e3645920dbddc78cb34b5
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-24 04:06:28 +0000

    ✅ Solved: power of two (cpp)

---
+++ solutions/cpp/math/power_of_two.cpp
@@ -0,0 +1,13 @@
+/*
+ * Power of Two Check
+ * Check if number is power of 2 using bit manipulation
+ * Category: math | Difficulty: Easy
+ * Date: 2026-02-24 | Author: Jay Singh
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// TODO: Implement Power of Two Check
