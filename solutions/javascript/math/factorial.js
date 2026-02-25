5410d76bf62ac091f2fac664aac36f09554eb8e2
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-25 07:50:15 +0000

    ✅ Solved: factorial (javascript)

---
+++ solutions/javascript/math/factorial.js
@@ -0,0 +1,8 @@
+/*
+ * Factorial
+ * Calculate factorial of a number using recursion and iteration
+ * Category: math | Difficulty: Easy
+ * Date: 2026-02-25 | Author: Jay Singh
+ */
+
+// TODO: Implement Factorial
