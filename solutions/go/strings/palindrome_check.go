2067989e6bc933a25b099e200fa8d95dd66fb29b
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-26 07:49:53 +0000

    ✅ Solved: palindrome check (go)

---
+++ solutions/go/strings/palindrome_check.go
@@ -0,0 +1,12 @@
+/*
+ * Palindrome Check
+ * Check if a string or number is a palindrome
+ * Category: strings | Difficulty: Easy
+ * Date: 2026-02-26 | Author: Jay Singh
+ */
+
+package main
+
+import "fmt"
+
+// TODO: Implement Palindrome Check
