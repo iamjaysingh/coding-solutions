fa27ed47ed6afa5efb42f89120bae787ff6cdba0
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-26 04:04:56 +0000

    ✅ Solved: insertion sort (javascript)

---
+++ solutions/javascript/sorting/insertion_sort.js
@@ -0,0 +1,8 @@
+/*
+ * Insertion Sort
+ * Implement insertion sort algorithm
+ * Category: sorting | Difficulty: Easy
+ * Date: 2026-02-26 | Author: Jay Singh
+ */
+
+// TODO: Implement Insertion Sort
