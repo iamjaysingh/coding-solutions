35af33d3a2f1527a7ab3d44f64fac1c12f6e762e
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-27 04:01:17 +0000

    ✅ Solved: binary search (javascript)

---
+++ solutions/javascript/searching/binary_search.js
@@ -0,0 +1,8 @@
+/*
+ * Binary Search
+ * Implement binary search iterative and recursive
+ * Category: searching | Difficulty: Easy
+ * Date: 2026-02-27 | Author: Jay Singh
+ */
+
+// TODO: Implement Binary Search
