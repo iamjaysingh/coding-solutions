bb3bb75c6b49aa9013bdfcd9735a18bdff894e29
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-02-28 03:44:32 +0000

    ✅ Solved: binary search (go)

---
+++ solutions/go/searching/binary_search.go
@@ -0,0 +1,12 @@
+/*
+ * Binary Search
+ * Implement binary search iterative and recursive
+ * Category: searching | Difficulty: Easy
+ * Date: 2026-02-28 | Author: Jay Singh
+ */
+
+package main
+
+import "fmt"
+
+// TODO: Implement Binary Search
