842dc9d9c3eaa4c7388f4b3bba7a7e0bd8d83c24
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-01 07:30:50 +0000

    ✅ Solved: pattern printing (python)

---
+++ solutions/python/basics/pattern_printing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Star Pattern Printing
+Print various star patterns (pyramid, diamond)
+Category: basics | Difficulty: Easy
+Date: 2026-03-01 | Author: Jay Singh
+"""
+
+// TODO: Implement Star Pattern Printing
