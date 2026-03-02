0e7f6186ecd473dd5d4f13b09adf5f97d2ff817f
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-02 07:47:13 +0000

    ✅ Solved: insertion sort (c)

---
+++ solutions/c/sorting/insertion_sort.c
@@ -0,0 +1,11 @@
+/*
+ * Insertion Sort
+ * Implement insertion sort algorithm
+ * Category: sorting | Difficulty: Easy
+ * Date: 2026-03-02 | Author: Jay Singh
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: Implement Insertion Sort
