227054599aeebe0f477f1a3ab3a65d7ecaf17634
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-02 04:01:43 +0000

    ✅ Solved: climbing stairs (python)

---
+++ solutions/python/dynamic-programming/climbing_stairs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Climbing Stairs
+Count ways to climb n stairs (1 or 2 steps)
+Category: dynamic-programming | Difficulty: Easy
+Date: 2026-03-02 | Author: Jay Singh
+"""
+
+// TODO: Implement Climbing Stairs
