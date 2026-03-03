9b621238a869380109b8dfc9c4695e7163f350a8
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-03 07:40:00 +0000

    ✅ Solved: counting sort (java)

---
+++ solutions/java/sorting/counting_sort.java
@@ -0,0 +1,10 @@
+/*
+ * Counting Sort
+ * Implement counting sort for integers
+ * Category: sorting | Difficulty: Medium
+ * Date: 2026-03-03 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Counting Sort
