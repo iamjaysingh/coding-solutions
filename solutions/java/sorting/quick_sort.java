6a06e1cf04edecc6dedddb978bfa3ec565ce7793
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-03 04:03:07 +0000

    ✅ Solved: quick sort (java)

---
+++ solutions/java/sorting/quick_sort.java
@@ -0,0 +1,10 @@
+/*
+ * Quick Sort
+ * Implement quick sort with partition
+ * Category: sorting | Difficulty: Medium
+ * Date: 2026-03-03 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Quick Sort
