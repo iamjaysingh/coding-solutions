923b0d00d481b5cf560e803a135aef003bec2f56
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-04 03:56:44 +0000

    ✅ Solved: insertion sort (java)

---
+++ solutions/java/sorting/insertion_sort.java
@@ -0,0 +1,10 @@
+/*
+ * Insertion Sort
+ * Implement insertion sort algorithm
+ * Category: sorting | Difficulty: Easy
+ * Date: 2026-03-04 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Insertion Sort
