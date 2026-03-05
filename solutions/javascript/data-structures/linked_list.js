8038990e6fddbbbe9f8afcefb732e8c7fccd8007
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-05 04:00:49 +0000

    ✅ Solved: linked list (javascript)

---
+++ solutions/javascript/data-structures/linked_list.js
@@ -0,0 +1,8 @@
+/*
+ * Linked List
+ * Implement singly linked list with insert, delete, search
+ * Category: data-structures | Difficulty: Medium
+ * Date: 2026-03-05 | Author: Jay Singh
+ */
+
+// TODO: Implement Linked List
