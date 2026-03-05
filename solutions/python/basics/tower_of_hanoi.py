4ffd771bd5b61bfa1b248bb28131bcdf3428926a
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-05 07:39:40 +0000

    ✅ Solved: tower of hanoi (python)

---
+++ solutions/python/basics/tower_of_hanoi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Tower of Hanoi
+Solve Tower of Hanoi using recursion
+Category: basics | Difficulty: Medium
+Date: 2026-03-05 | Author: Jay Singh
+"""
+
+// TODO: Implement Tower of Hanoi
