bc6fd57a7a49c8fdc4d08ff2b06a181d95dc35bc
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-07 07:26:00 +0000

    ✅ Solved: coin change (java)

---
+++ solutions/java/dynamic-programming/coin_change.java
@@ -0,0 +1,10 @@
+/*
+ * Coin Change Problem
+ * Find minimum coins to make change
+ * Category: dynamic-programming | Difficulty: Medium
+ * Date: 2026-03-07 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Coin Change Problem
