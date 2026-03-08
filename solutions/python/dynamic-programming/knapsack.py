d10faea80f8deabb16c14c7ddfcdfb60db90e093
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-08 07:28:46 +0000

    ✅ Solved: knapsack (python)

---
+++ solutions/python/dynamic-programming/knapsack.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+0/1 Knapsack Problem
+Solve knapsack problem using dynamic programming
+Category: dynamic-programming | Difficulty: Hard
+Date: 2026-03-08 | Author: Jay Singh
+"""
+
+// TODO: Implement 0/1 Knapsack Problem
