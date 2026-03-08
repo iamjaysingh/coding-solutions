27c1f2536ef01a2c11c323debfbe91f6a8d7acf1
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-08 03:59:37 +0000

    ✅ Solved: temperature converter (javascript)

---
+++ solutions/javascript/basics/temperature_converter.js
@@ -0,0 +1,8 @@
+/*
+ * Temperature Converter
+ * Convert between Celsius, Fahrenheit, and Kelvin
+ * Category: basics | Difficulty: Easy
+ * Date: 2026-03-08 | Author: Jay Singh
+ */
+
+// TODO: Implement Temperature Converter
