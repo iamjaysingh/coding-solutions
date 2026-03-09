a01595901c975a6aace6625b4a9583046d62f24a
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-09 04:04:49 +0000

    ✅ Solved: linear search (javascript)

---
+++ solutions/javascript/searching/linear_search.js
@@ -0,0 +1,8 @@
+/*
+ * Linear Search
+ * Implement linear search algorithm
+ * Category: searching | Difficulty: Easy
+ * Date: 2026-03-09 | Author: Jay Singh
+ */
+
+// TODO: Implement Linear Search
