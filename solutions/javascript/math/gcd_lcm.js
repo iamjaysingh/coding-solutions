20d8cbc2a71f524f885cc76f59e60c07587a1be7
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-09 07:48:28 +0000

    ✅ Solved: gcd lcm (javascript)

---
+++ solutions/javascript/math/gcd_lcm.js
@@ -0,0 +1,8 @@
+/*
+ * GCD and LCM
+ * Find GCD and LCM using Euclidean algorithm
+ * Category: math | Difficulty: Easy
+ * Date: 2026-03-09 | Author: Jay Singh
+ */
+
+// TODO: Implement GCD and LCM
