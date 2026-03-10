a6db6908761dd6cbfe4f9f90626e15d8f2600f9e
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-10 03:57:28 +0000

    ✅ Solved: binary search tree (go)

---
+++ solutions/go/data-structures/binary_search_tree.go
@@ -0,0 +1,12 @@
+/*
+ * Binary Search Tree
+ * Implement BST with insert, search, delete, traversals
+ * Category: data-structures | Difficulty: Medium
+ * Date: 2026-03-10 | Author: Jay Singh
+ */
+
+package main
+
+import "fmt"
+
+// TODO: Implement Binary Search Tree
