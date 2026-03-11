7e63acaf30e00e581202c227301d055135392318
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-11 03:58:26 +0000

    ✅ Solved: bubble sort (c)

---
+++ solutions/c/sorting/bubble_sort.c
@@ -0,0 +1,11 @@
+/*
+ * Bubble Sort
+ * Implement bubble sort algorithm with optimization
+ * Category: sorting | Difficulty: Easy
+ * Date: 2026-03-11 | Author: Jay Singh
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: Implement Bubble Sort
