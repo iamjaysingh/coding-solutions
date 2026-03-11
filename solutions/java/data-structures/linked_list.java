6c7babbd71536ed4b087e7d301d5f249cbdbc136
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-11 07:41:02 +0000

    ✅ Solved: linked list (java)

---
+++ solutions/java/data-structures/linked_list.java
@@ -0,0 +1,10 @@
+/*
+ * Linked List
+ * Implement singly linked list with insert, delete, search
+ * Category: data-structures | Difficulty: Medium
+ * Date: 2026-03-11 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Linked List
