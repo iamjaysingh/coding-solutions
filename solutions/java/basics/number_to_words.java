0ef85ac839372aa7c99f2666e9e26c91460aa6fc
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-13 07:42:29 +0000

    ✅ Solved: number to words (java)

---
+++ solutions/java/basics/number_to_words.java
@@ -0,0 +1,10 @@
+/*
+ * Number to Words
+ * Convert number to English words
+ * Category: basics | Difficulty: Medium
+ * Date: 2026-03-13 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Number to Words
