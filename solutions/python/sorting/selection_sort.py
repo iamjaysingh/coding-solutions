95fa060f3a0a8a33fb39d103dc97770f1ea19292
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-14 03:59:24 +0000

    ✅ Solved: selection sort (python)

---
+++ solutions/python/sorting/selection_sort.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Selection Sort
+Implement selection sort algorithm
+Category: sorting | Difficulty: Easy
+Date: 2026-03-14 | Author: Jay Singh
+"""
+
+// TODO: Implement Selection Sort
