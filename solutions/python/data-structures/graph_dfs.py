6377029238fad1ae7cd3b3b820989f294a8b1b63
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-15 04:21:31 +0000

    ✅ Solved: graph dfs (python)

---
+++ solutions/python/data-structures/graph_dfs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+DFS Graph Traversal
+Implement Depth-First Search for graphs
+Category: data-structures | Difficulty: Medium
+Date: 2026-03-15 | Author: Jay Singh
+"""
+
+// TODO: Implement DFS Graph Traversal
