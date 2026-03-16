4df79524d645a8f2d92a6c30c25d5e4ceb1a2595
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-16 04:28:33 +0000

    ✅ Solved: temperature converter (python)

---
+++ solutions/python/basics/temperature_converter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Temperature Converter
+Convert between Celsius, Fahrenheit, and Kelvin
+Category: basics | Difficulty: Easy
+Date: 2026-03-16 | Author: Jay Singh
+"""
+
+// TODO: Implement Temperature Converter
