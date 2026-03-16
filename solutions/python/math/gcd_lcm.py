d624b21a1c9b87fa643e13b102094036e3dc1ada
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-16 08:06:23 +0000

    ✅ Solved: gcd lcm (python)

---
+++ solutions/python/math/gcd_lcm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+GCD and LCM
+Find GCD and LCM using Euclidean algorithm
+Category: math | Difficulty: Easy
+Date: 2026-03-16 | Author: Jay Singh
+"""
+
+// TODO: Implement GCD and LCM
