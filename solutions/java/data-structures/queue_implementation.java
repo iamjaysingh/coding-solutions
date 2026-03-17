fd1675bf2e785098cf5872480fa47691d31437e0
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-17 04:05:42 +0000

    ✅ Solved: queue implementation (java)

---
+++ solutions/java/data-structures/queue_implementation.java
@@ -0,0 +1,10 @@
+/*
+ * Queue Implementation
+ * Implement queue with enqueue, dequeue operations
+ * Category: data-structures | Difficulty: Easy
+ * Date: 2026-03-17 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Queue Implementation
