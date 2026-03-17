7ff2ac837fdf0cbc2d3e52735f2d4fd4f5aa51ea
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-17 07:52:03 +0000

    ✅ Solved: prime numbers (javascript)

---
+++ solutions/javascript/math/prime_numbers.js
@@ -0,0 +1,8 @@
+/*
+ * Prime Number Generator
+ * Find prime numbers using Sieve of Eratosthenes
+ * Category: math | Difficulty: Easy
+ * Date: 2026-03-17 | Author: Jay Singh
+ */
+
+// TODO: Implement Prime Number Generator
