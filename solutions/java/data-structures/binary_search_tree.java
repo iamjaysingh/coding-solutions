650bdcd09929ab732554396025d178f36589988f
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-18 07:49:53 +0000

    ✅ Solved: binary search tree (java)

---
+++ solutions/java/data-structures/binary_search_tree.java
@@ -0,0 +1,10 @@
+/*
+ * Binary Search Tree
+ * Implement BST with insert, search, delete, traversals
+ * Category: data-structures | Difficulty: Medium
+ * Date: 2026-03-18 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Binary Search Tree
