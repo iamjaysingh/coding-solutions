0fc0a7ba19b03c9966d0e2e998af4222c8161a31
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-19 07:44:44 +0000

    ✅ Solved: binary search (python)

---
+++ solutions/python/searching/binary_search.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Binary Search
+Implement binary search iterative and recursive
+Category: searching | Difficulty: Easy
+Date: 2026-03-19 | Author: Jay Singh
+"""
+
+// TODO: Implement Binary Search
