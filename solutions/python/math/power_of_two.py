f467aedba929ceac44970bea46c0da724e5a440d
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-19 04:10:00 +0000

    ✅ Solved: power of two (python)

---
+++ solutions/python/math/power_of_two.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Power of Two Check
+Check if number is power of 2 using bit manipulation
+Category: math | Difficulty: Easy
+Date: 2026-03-19 | Author: Jay Singh
+"""
+
+// TODO: Implement Power of Two Check
