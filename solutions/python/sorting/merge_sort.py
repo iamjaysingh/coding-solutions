d913515ab752221d42650b2fd2f6774f166c3534
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-20 07:43:21 +0000

    ✅ Solved: merge sort (python)

---
+++ solutions/python/sorting/merge_sort.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Merge Sort
+Implement merge sort using divide and conquer
+Category: sorting | Difficulty: Medium
+Date: 2026-03-20 | Author: Jay Singh
+"""
+
+// TODO: Implement Merge Sort
