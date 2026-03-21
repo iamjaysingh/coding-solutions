5750445fa515d50eff3409ea6515d38ee48e468b
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-21 07:30:53 +0000

    ✅ Solved: merge sort (java)

---
+++ solutions/java/sorting/merge_sort.java
@@ -0,0 +1,10 @@
+/*
+ * Merge Sort
+ * Implement merge sort using divide and conquer
+ * Category: sorting | Difficulty: Medium
+ * Date: 2026-03-21 | Author: Jay Singh
+ */
+
+import java.util.*;
+
+// TODO: Implement Merge Sort
