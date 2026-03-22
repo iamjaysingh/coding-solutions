19f39f08434fa5bb703db71a6a136eb1de30a116
Jay Singh <jayrakeshsingh8796@gmail.com>
2026-03-22 07:36:06 +0000

    ✅ Solved: binary search tree (javascript)

---
+++ solutions/javascript/data-structures/binary_search_tree.js
@@ -0,0 +1,8 @@
+/*
+ * Binary Search Tree
+ * Implement BST with insert, search, delete, traversals
+ * Category: data-structures | Difficulty: Medium
+ * Date: 2026-03-22 | Author: Jay Singh
+ */
+
+// TODO: Implement Binary Search Tree
